c2278bf7be820dca15d3e5df0cb1f9edbedecd80
thaistudando <44445105+thaistudando@users.noreply.github.com>
2018-11-26 10:36:22 -0200

    Update MyTree.java

--- MyTree.java
+++ MyTree.java
@@ -1,3 +1,29 @@
-class MyTree<T> extends BinSearchTree<T> {
+class MyTree<T> extends AVLTree  
+{
+    /**
+    * Recursive function to balance the tree
+    */
+    @Override
+    public BinTreeNode rebalance(BinTreeNode<T> node) {
+        if (node != null) {
+            node.setRightChild((rebalance(node.getRightChild())));
+            node.setLeftChild((rebalance(node.getLeftChild())));
 
+            if (newBalancingFactor(node) == 3) {
+                if (newBalancingFactor(node.getLeftChild()) > 0) { 
+                    super.rotateRight(node);
+                } else {
+                    super.rotateLR(node);
+                }
+            } else if (newBalancingFactor(node) == -3) {
+                if (newBalancingFactor(node.getRightChild()) < 0) 
+                {
+                    super.rotateLeft(node);
+                } else {
+                    super.rotateRL(node);
+                }
+            }
+        }
+        return node;
+    }
 }
